50059367c390c5d957f6731270740ae487f9c79f
YogeshwarPichewar <yogeshpichewar@gmail.com>
2022-09-28 11:03:13 +0530

    one more class

---
+++ Strings/src/FactorialSeries.java
@@ -0,0 +1,21 @@
+
+public class FactorialSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a =0;
+		int b=1;
+		int sum=0 ;
+		int  i=1;
+		while(i <=3) {
+			System.out.print(a+" ");
+			sum =a+b;
+			a=b;
+			b=sum;
+			i++;
+			
+		}
+	}
+
+}
