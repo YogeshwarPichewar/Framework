8f83a95c20762d3ec152240d1f4adc74b81883b6
YogeshwarPichewar <yogeshpichewar@gmail.com>
2022-09-29 08:37:55 +0530

    A2B3C4

---
+++ Strings/src/A2B3C4.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class frequency {
+
+	public static void main(String[] args) {
+		
+		String str = "a2b3c4";
+for(int i=0;i<str.length();i++){
+if(Character.isAlphabetic(s.charAt(i))){
+System.out.print(s.charAt(i));
+}else {
+int a = Character.getNumericValue(s.charAt(i));
+for(int j =1; j <a; j++) {
+System.out.print(s.charAt(i-1));
+}
+}}
+		
+}}
