be71af84a037b9a4da9bd5859b079ba51ead9172
YogeshwarPichewar <yogeshpichewar@gmail.com>
2022-09-29 09:40:49 +0530

    logic

--- Strings/src/A2B3C4.java
+++ Strings/src/A2B3C4.java
@@ -1,19 +1,20 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class frequency {
+public class A2B3C4 {
 
 	public static void main(String[] args) {
 		
-		String str = "a2b3c4";
-for(int i=0;i<str.length();i++){
-if(Character.isAlphabetic(s.charAt(i))){
-System.out.print(s.charAt(i));
-}else {
-int a = Character.getNumericValue(s.charAt(i));
-for(int j =1; j <a; j++) {
-System.out.print(s.charAt(i-1));
-}
+		String s = "a2b3c4";
+		for(int i =0; i < s.length(); i++) {
+			if(Character.isAlphabetic(s.charAt(i))){
+			System.out.print(s.charAt(i));
+			}else {
+			int a = Character.getNumericValue(s.charAt(i));
+			for(int j =1; j <a; j++) {
+			System.out.print(s.charAt(i-1));
+			}
+			}
 }}
 		
-}}
+}
