bf8bace0bec5bc327c1a517283f1bacc0f17849e
YogeshwarPichewar <yogeshpichewar@gmail.com>
2022-09-29 08:16:11 +0530

    changed classname

---
+++ Strings/src/Pattern.java
@@ -0,0 +1,17 @@
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		int sum=0;
+		while(i<=2) {
+			
+				sum = sum + (2* i );
+				i++;
+			
+		}
+System.out.print("sum of first 20 even numbers is"+" "+ sum);
+	}
+
+}
